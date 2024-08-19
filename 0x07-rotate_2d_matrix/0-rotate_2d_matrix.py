de1ddb8057a7b4c2563e1f8e6603235c20ec4632
sujh8 <suhajhm@yahoo.com>
2024-08-19 05:20:05 -0700

    tt

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,29 +1,14 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
-"""
+"""Rotate 2D Matrix"""
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates an m by n 2D matrix in place.
-    """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+    """a method to rotate 2D matrix 90 degrees clockwise"""
+    N = len(matrix)
+    for x in range(0, int(N / 2)):
+        for y in range(x, N-x-1):
+            temp = matrix[x][y]
+            matrix[x][y] = matrix[N-1-y][x]
+            matrix[N-1-y][x] = matrix[N-1-x][N-1-y]
+            matrix[N-1-x][N-1-y] = matrix[y][N-1-x]
+            matrix[y][N-1-x] = temp
